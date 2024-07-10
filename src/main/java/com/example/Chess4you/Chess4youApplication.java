0938c89f67c9f66c6e3d6dbf8f10272b2b1487fa
xeonowsky <klaudiusz599@gmail.com>
2024-07-10 13:49:26 +0200

    Initial commit

---
+++ src/main/java/com/example/Chess4you/Chess4youApplication.java
@@ -0,0 +1,13 @@
+package com.example.Chess4you;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Chess4youApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Chess4youApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/Chess4you/Chess4youApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.Chess4you;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Chess4youApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
